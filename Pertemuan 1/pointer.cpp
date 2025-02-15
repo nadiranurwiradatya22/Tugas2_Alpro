f6933bc57887f6f305a4cc2732b3779fa4a3bc28
nadiranurwiradatya22 <nadiranurwiradatya22@gmail.com>
2025-02-15 14:40:06 +0700

    perubahan ke 2

--- Pertemuan 1/pointer.cpp
+++ Pertemuan 1/pointer.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
+int main() { //pointer = * -- Refenrence = & 
+//* menyimpan alamat dari angka
+//&menyimpan 
+int angka[5] = {87,56,93,70,100};
+cout << *angka[0] << endl; 
+update ke 2
 
-int main(){
-    int o = 10;
-    cout << o << endl;
 }
